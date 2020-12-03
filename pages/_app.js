0a2cd66e883ceecdc5699581c1e22b4e03d939b8
Chistikov <vlad.chistikov998@gmail.com>
2020-12-03 05:23:06 +0200

    Added font

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
+import Head from "next/head";
+import Link from "next/link";
+
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="preload" href="../public/fonts/Lato/Lato-Bold.ttf" as="font" crossOrigin="" />
+        <link rel="preload" href="../public/fonts/Lato/Lato-Light.ttf" as="font" crossOrigin="" />
+        <link rel="preload" href="../public/fonts/Lato/Lato-Regular.ttf" as="font" crossOrigin="" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
