c0dde88f600931879ece7c25d15cbd73b7aba53c
Chistikov <vlad.chistikov998@gmail.com>
2020-12-08 19:20:19 +0200

    Fixed fonts pathes

--- pages/_app.js
+++ pages/_app.js
@@ -8,9 +8,9 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
-        <link rel="preload" href="../public/fonts/Lato/Lato-Bold.ttf" as="font" crossOrigin="" />
-        <link rel="preload" href="../public/fonts/Lato/Lato-Light.ttf" as="font" crossOrigin="" />
-        <link rel="preload" href="../public/fonts/Lato/Lato-Regular.ttf" as="font" crossOrigin="" />
+        <link rel="preload" href="/fonts/Lato/Lato-Bold.ttf" as="font" crossOrigin="" />
+        <link rel="preload" href="/fonts/Lato/Lato-Light.ttf" as="font" crossOrigin="" />
+        <link rel="preload" href="/fonts/Lato/Lato-Regular.ttf" as="font" crossOrigin="" />
       </Head>
       <Component {...pageProps} />
     </>
